464e344490a80b79e1251957a26185be41f204f4
araobp <araobp@users.noreply.github.com>
2025-12-06 16:08:54 +0900

    feat: Add new materials and recordings
    This commit introduces new materials for the robot and adds recordings of the image sequence. It also includes updates to the RobotTest scene and script.

--- robotics/Assets/Scripts/RobotTest.cs
+++ robotics/Assets/Scripts/RobotTest.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.XR;
+using System;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
+using Newtonsoft.Json.Bson;
 
 public class RobotTest : MonoBehaviour
 {
+
+    [SerializeField] GameObject work;
 
     [SerializeField] Button buttonSwingR;
     [SerializeField] Button buttonSwingL;
@@ -58,7 +65,10 @@ public class RobotTest : MonoBehaviour
 
         SetupButtonEvents(buttonHandR, (isPressed) => isHandButtonRPressed = isPressed);
         SetupButtonEvents(buttonHandL, (isPressed) => isHandButtonLPressed = isPressed);
+
+        IKTest();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -142,4 +152,59 @@ public class RobotTest : MonoBehaviour
         Debug.Log("Swing Clicked");
     }
 
+
+    /* This function is my original implementation of Inverse Kinematics for the robot arm.
+     * It calculates the necessary joint angles to position the end effector (hand)
+     * at the target position defined by the 'work' GameObject.
+     * The calculations are based on the geometric relationships of the robot arm's segments.
+     */ 
+    public void IKTest()
+    {
+        // Constants
+        const float AB = 0.169f;
+        const float Bs = 0.273f;
+        const float HANDSIZE = 0.325f;
+        const float GF = HANDSIZE - Bs;
+        const float FE = 0.49727f;
+        const float ED = 0.70142f;
+
+        // Work position
+        Vector3 A = work.transform.position;
+        Debug.Log("Work position: " + A.ToString("F4"));
+        
+        float theta1 = Mathf.Atan2(A.z, A.x);
+        Debug.Log("Theta1: " + (theta1 * Mathf.Rad2Deg).ToString("F4"));
+
+        float AC = A.x / Mathf.Cos(theta1);
+        float theta3 = Mathf.Asin(AB/AC);
+        Debug.Log("Theta3: " + (theta3 * Mathf.Rad2Deg).ToString("F4"));
+
+        float BC = AC * Mathf.Cos(theta3);
+        Debug.Log("BC: " + BC.ToString("F4"));
+
+        float theta2 = theta1 - theta3;
+        Debug.Log("Theta2: " + (theta2 * Mathf.Rad2Deg).ToString("F4"));
+
+        Vector3 B = new Vector3(BC * Mathf.Cos(theta2), A.y, BC * Mathf.Sin(theta2));
+        Vector3 G = new Vector3(B.x, B.y + Bs, B.z);
+
+        float r = Mathf.Sqrt(BC * BC  + GF * GF);
+        Debug.Log("r: " + r.ToString("F4"));
+
+        // Cosine theorem
+        float theat6 = Mathf.Acos((FE * FE - ED * ED - r * r) / (- 2 * ED * r));
+        float theat7 = Mathf.Acos((r * r - FE * FE - ED * ED) / (- 2 * FE * ED));
+        Debug.Log("Theta6: " + (theat6 * Mathf.Rad2Deg).ToString("F4"));
+        Debug.Log("Theta7: " + (theat7 * Mathf.Rad2Deg).ToString("F4"));
+
+        float theat5 = Mathf.Atan2(GF, BC);
+        float theat4 = theat5 + theat6;
+        Debug.Log("Theta4: " + (theat4 * Mathf.Rad2Deg).ToString("F4"));
+        Debug.Log("Theta5: " + (theat5 * Mathf.Rad2Deg).ToString("F4"));
+
+        // Set rotations of the bones of the robot
+        boneSwing.transform.localEulerAngles = new Vector3(theta2 * Mathf.Rad2Deg, 90f, 90f);
+        boneBoom.transform.localEulerAngles = new Vector3(theat4 * Mathf.Rad2Deg,90f,90f);
+        boneArm.transform.localEulerAngles = new Vector3(-theat7 * Mathf.Rad2Deg,-90f,90f);
+    }
 }
